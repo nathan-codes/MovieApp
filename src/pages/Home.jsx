73a73bd101033cb6f334cf3fcc6b7f9e4b7d26b1
Nathan Turkson <nathanturkson@gmail.com>
2024-05-22 21:32:01 +0000

    ui updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
+import BannerHome from "../components/BannerHome";
+import HorizontalScrollCardGroup from "../components/HorizontalScrollCardGroup";
 
-import BannerHome from "../components/BannerHome"
-import HorizontalScrollCardGroup from "../components/HorizontalScrollCardGroup"
+import { useSelector } from "react-redux";
+
 const Home = () => {
+  const data = useSelector((state) => {
+    return state.movieoData.bannerData;
+  });
   return (
     <div className="">
       <BannerHome />
-      <HorizontalScrollCardGroup />
+      <HorizontalScrollCardGroup title={"Trending Now"} data={data} />
+ 
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
